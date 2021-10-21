a7e4c3d8634e71993e7981b2acae8ca30093a172
Jonhyz <92896542+Jonhyz@users.noreply.github.com>
2021-10-21 17:29:53 -0500

    Update CompraTick.java

--- src/modelo/CompraTick.java
+++ src/modelo/CompraTick.java
@@ -1,84 +1,81 @@
 
 package modelo;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
-
+import javax.swing.ListSelectionModel;
+ 
 class CompraTick extends JFrame{
-    JPanel panel = new JPanel(); //genera el panel
-    private final JComboBox<String> numboletos,numboletos2;
-    private static final String[]numBoletos={"","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15",
-                                              "16","17","18","19","20","21","22"};
+    
+    JPanel panel  = new JPanel();
     
     public CompraTick(){
-        super("Venta de Tickets");     //nombre de la ventana          
         
-        setLayout(new FlowLayout());
+        super("Tickets");
+        panel.setLayout(null);
         getContentPane().add(panel);
         componentes();
         
-        numboletos = new JComboBox<String>(numBoletos);
-        numboletos.setBounds(10,30,150,40);
-        numboletos.setMaximumRowCount(7);
-        
-        numboletos.addItemListener(new ItemListener() {
-
-            @Override
-            public void itemStateChanged(ItemEvent evento) {
-            }
-        });
-        add(numboletos);
-        
-        numboletos2 = new JComboBox<String>(numBoletos);
-        numboletos2.setBounds(10,90,150,40);
-        numboletos2.setMaximumRowCount(7);
-        
-        numboletos2.addItemListener(new ItemListener() {
-
-            @Override
-            public void itemStateChanged(ItemEvent evento) {
-            }
-        });
-        add(numboletos2);
     }
-
-    private void componentes() {
+    private void componentes(){
         etiquetas();
-        total();
-        botones();
+        jcomobox();
+        botones(); 
+        campotexto();
+    }
+    private void botones(){
+        JButton boton1 = new JButton("Comprar");     
+        boton1.setBounds(255,10,100,25);
+        panel.add(boton1);
+        
+        JButton boton2 = new JButton("Cancelar"); 
+         boton2.setBounds(255,45,100,25);
+        panel.add(boton2);
+    }
+    private void jcomobox(){       
+        String numBoletos[] = {"","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22"};
+        JComboBox caja = new JComboBox(numBoletos);
+        caja.setBounds(135, 15, 60, 20);
+        panel.add(caja);
+        String numBoletos2[] = {"","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22"};
+        JComboBox caja1 = new JComboBox(numBoletos2);
+        caja1.setBounds(135, 45, 60, 20);
+        panel.add(caja1);
+    }
+    private void etiquetas(){
+        JLabel etiqueta1 = new JLabel("Boletos Adultos: ");
+        etiqueta1.setBounds(25,15,200,20);
+        panel.add(etiqueta1);
+        
+        JLabel etiqueta2 = new JLabel("Boletos Niños: ");
+        etiqueta2.setBounds(25,45,200,20);  //primer numero sangria, 2do arriba abajo, posiicones
+        panel.add(etiqueta2);
+        
+        JLabel etiqueta3 = new JLabel("Total: ");
+        etiqueta3.setBounds(25,75,200,20);  //primer numero sangria, 2do arriba abajo, posiicones
+        panel.add(etiqueta3);
+        
+        JLabel etiqueta4 = new JLabel("TICKETS");
+        etiqueta4.setBounds(175,100,200,20);  //primer numero sangria, 2do arriba abajo, posiicones
+        panel.add(etiqueta4);
     }
 
-    private void etiquetas() {
-        JLabel adultos = new JLabel("Adultos: ");
-        adultos.setBounds(160,38,20,20);
-        panel.add(adultos);
-        JLabel ninos = new JLabel("Niños: ");
-        ninos.setBounds(160,90,20,20);
-        panel.add(ninos);
-        JLabel total = new JLabel("TOTAL");
-        total.setBounds(160,90,20,20);
-        panel.add(total);
-    }
 
-    private void total() {
-        JTextField campotexto1 = new JTextField("$",20);
-        campotexto1.setBounds(180,35,50,30);
+     private void campotexto(){
+        JTextField campotexto1 = new JTextField("$ ");
+        campotexto1.setBounds(80,75,100,20);
         panel.add(campotexto1);
+        JTextField campotexto2 = new JTextField(" ");
+        campotexto2.setBounds(25,125,325,100);
+        panel.add(campotexto2);
+
     }
     
-    private void botones() {
-        JButton boton1 = new JButton("Comprar");  
-        boton1.setPreferredSize(new Dimension(120,25));
-        add(boton1);
-        
-        JButton boton2 = new JButton("Cancelar");
-        boton2.setPreferredSize(new Dimension(120,25));
-        add(boton2);
-    }
+   
 }
